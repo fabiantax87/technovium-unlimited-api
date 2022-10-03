fe702afde0f5cf3d16f40d4501b3be284d0be09d
fabiantax87 <fabiantax87@users.noreply.github.com>
2022-10-03 12:46:53 +0200

    added new fields in account schema

--- server/models/Account.js
+++ server/models/Account.js
@@ -1,8 +1,32 @@
 const mongoose = require("mongoose");
 
 const accountSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
   username: {
     type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date_of_birth: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Object,
     required: true,
   },
 });
