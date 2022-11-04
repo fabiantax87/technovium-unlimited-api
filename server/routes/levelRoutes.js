eab2b32123c8406c5567fa2a7ebc66e50400c88c
tychovr <68756557+tychovr@users.noreply.github.com>
2022-11-04 13:34:52 +0100

    Revert "Added limit to levels (#16)"
    This reverts commit 25a08621f2a38da413c4fa4507329a4b4de2f2a0.

--- server/routes/levelRoutes.js
+++ server/routes/levelRoutes.js
@@ -5,7 +5,7 @@ const levelController = require("../controllers/levelController");
 /**
  * Level routes
  */
-router.get("/api/levels/:limit", levelController.getLevels);
+router.get("/api/levels", levelController.getLevels);
 router.get("/api/levels/:game_id", levelController.getGameLevels);
 
 module.exports = router;
